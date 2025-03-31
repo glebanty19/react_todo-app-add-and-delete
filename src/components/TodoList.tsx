27450a0fc09e97d0695250e5abdee93fdef77074
Hleb Bondar <hlebbondar2004@gmail.com>
2025-04-01 00:37:09 +0300

    add files

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Todo } from '../types/Todo';
+import { TodoItem } from './TodoItem';
+import { FilterStatus } from '../types/FilterStatus';
+
+type Props = {
+  todos: Todo[];
+  filterStatus: FilterStatus;
+  isLoading: boolean;
+  onDelete: (id: number) => void;
+  deletingTodoIds: number[];
+  tempTodo: Todo | null;
+};
+
+export const TodoList: React.FC<Props> = ({
+  todos,
+  filterStatus,
+  isLoading,
+  onDelete,
+  deletingTodoIds,
+  tempTodo,
+}) => {
+  const filteredTodos = todos.filter(todo => {
+    if (filterStatus === FilterStatus.All) {
+      return true;
+    }
+
+    if (filterStatus === FilterStatus.Active) {
+      return !todo.completed;
+    }
+
+    if (filterStatus === FilterStatus.Completed) {
+      return todo.completed;
+    }
+
+    return true;
+  });
+
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {isLoading && !todos.length ? (
+        <div className="modal overlay is-active">
+          <div className="modal-background has-background-white-ter" />
+          <div className="loader" />
+        </div>
+      ) : (
+        <>
+          {filteredTodos.map(todo => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onDelete={onDelete}
+              isDeleting={deletingTodoIds.includes(todo.id)}
+            />
+          ))}
+
+          {tempTodo && (
+            <TodoItem todo={tempTodo} onDelete={() => {}} isLoading={true} />
+          )}
+        </>
+      )}
+    </section>
+  );
+};
